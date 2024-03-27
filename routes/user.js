59112e1155a43e4b88ed896396fb3a89f68f6ce0
sherifyanni <163025160+sherifyanni@users.noreply.github.com>
2024-03-27 18:31:33 +0200

    Add files via upload

--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,33 @@
+const router = require("express").Router();
+const User = require("../models/login.model")
 
+
+router.route("/").get((req,res)=>{
+    User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json("Error: "+ err));
+});
+
+
+router.route("/add").post((req,res)=>{
+    const name = req.body.name;
+    const password = req.body.password;
+    const newName = new User(name)
+    const newPass = new User(password)
+
+
+
+
+
+    newName.save()
+    .then(() => res.json("Name added"))
+    .catch(err => res.status(400).json("Error: "+ err));
+
+
+    newPass.save()
+    .then(() => res.json("Pass added"))
+    .catch(err => res.status(400).json("Error: "+ err));
+});
+
+
+module.exports = router;
