6f191c59cce5c5d4a8e36a24dd559e65cd080a52
sherifyanni <163025160+sherifyanni@users.noreply.github.com>
2024-04-15 02:59:19 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,16 +1,21 @@
 const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+
+    },
     email: {
         type: String,
         unique: true,
+        required: true
     
     },
     password: {
         type: String,
-    },
-    token: {
-        type: String,
+        required: true,
     }
+    
 })
 
 
