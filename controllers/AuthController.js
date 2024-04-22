39329ec80f15ebc010ce5cd9d092cb4b7a8fb83e
Mohamed Elshamy <mohamedabdelhady88@gmail.com>
2024-04-22 11:36:46 +0200

    .

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,61 +1,74 @@
-const jwt = require("jsonwebtoken")
-const {expressjwt} = require("express-jwt")
-const bcrypt = require("bcrypt")
-const dotenv = require("dotenv")
-const Joi = require("joi")
+const jwt = require("jsonwebtoken");
+const { expressjwt } = require("express-jwt");
+const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
+const Joi = require("joi");
 // const sendEmail = require("../utils/sendEmail")
 dotenv.config();
-const express = require("express")
-const { user } = require("../models/user")
-const { vet } = require("../models/vet")
-const { shop } = require("../models/shop")
+const express = require("express");
+const { user, user, user } = require("../models/user");
+const { vet } = require("../models/vet");
+const { shop } = require("../models/shop");
 
-ACCESS_TOKEN_SECRET= "hello"
-REFRESH_TOKEN_SECRET= "helloAgain"
+ACCESS_TOKEN_SECRET = "hello";
+REFRESH_TOKEN_SECRET = "helloAgain";
 
+const app = express();
 
-const app = express()
+require("dotenv").config();
 
-require("dotenv").config()
+require("cookie-parser");
+app.use(express.json());
 
-require("cookie-parser")
-app.use(express.json())
+exports.signup = async (req, res) => {
+  try {
+    const { name, email, password, gender, age, adress } = req.body;
 
-
-
-
-exports.signup = async(req,res)=>{
-    try{
-        const{ name, email, password, gender, age, adress } = req.body
-        
-        
-        if(!(email && password && name && gender && age && adress)){
-            res.status(400).send("Please enter all fields")
-        }
-        const existentUser = await user.findOne({email})
-        if(existentUser){
-            return res.status(409).send("User already exists, please sign in instead")
-        }
-        encryptPass = await bcrypt.hash(password, 10);
-
-        
-        
-        const createUser = await user.create({name, email, password:encryptPass})
-      
-        res.status(200).json(createUser)
-    }catch(error){
-        res.status(400).json({error})
-    
+    if (!(email && password && name && gender && age && adress)) {
+      res.status(400).send("Please enter all fields");
     }
-    
-} 
+    const existentUser = await user.findOne({ email });
+    if (existentUser) {
+      return res
+        .status(409)
+        .send("User already exists, please sign in instead");
+    }
+    encryptPass = await bcrypt.hash(password, 10);
+
+    const createUser = await user.create({
+      name,
+      email,
+      password: encryptPass,
+    });
+
+    res.status(200).json(createUser);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
 
 exports.shopsignup = async (req, res) => {
   try {
-    const { username, password, email, address, phone, taxRegister, offerings } = req.body;
+    const {
+      username,
+      password,
+      email,
+      address,
+      phone,
+      taxRegister,
+      offerings,
+    } = req.body;
 
     // Check If The Input Fields are Valid
-    if (!username || !password || !email || !address || !phone || !taxRegister || !offerings  ) {
+    if (
+      !username ||
+      !password ||
+      !email ||
+      !address ||
+      !phone ||
+      !taxRegister ||
+      !offerings
+    ) {
       return res
         .status(400)
         .json({ message: "Please Input Username and Password" });
@@ -94,14 +107,20 @@ exports.shopsignup = async (req, res) => {
   }
 };
 
-
-
 exports.vetsignUp = async (req, res) => {
   try {
-    const { username, password, email, address, phone, syndicateCard } = req.body;
+    const { username, password, email, address, phone, syndicateCard } =
+      req.body;
 
     // Check If The Input Fields are Valid
-    if (!username || !password || !email || !address || !phone || !syndicateCard  ) {
+    if (
+      !username ||
+      !password ||
+      !email ||
+      !address ||
+      !phone ||
+      !syndicateCard
+    ) {
       return res
         .status(400)
         .json({ message: "Please Input Username and Password" });
@@ -125,7 +144,7 @@ exports.vetsignUp = async (req, res) => {
       email,
       address,
       phone,
-      syndicateCard
+      syndicateCard,
     });
 
     await newUser.save();
@@ -139,234 +158,212 @@ exports.vetsignUp = async (req, res) => {
   }
 };
 
-exports.signin = async(req, res)=>{
-  
-    try{
-        const{ email, password } = req.body
-        const checkUser = await user.findOne({ email })
-        const checkShop = await shop.findOne({ email })
-        const checkVet = await vet.findOne({ email })
-        console.log(checkShop)
-        console.log(checkUser)
-        if (!checkUser && !checkShop && !checkVet){
-            return res.json({status: 'error', error: 'Invalid username or password'})
-        }
-        console.log(password)
-        if(checkUser){
-          const comparePass = await bcrypt.compare(password, checkUser.password)
+exports.signin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const checkUser = await user.findOne({ email });
+    const checkShop = await shop.findOne({ email });
+    const checkVet = await vet.findOne({ email });
+    console.log(checkShop);
+    console.log(checkUser);
+    if (!checkUser && !checkShop && !checkVet) {
+      return res.json({
+        status: "error",
+        error: "Invalid username or password",
+      });
+    }
+    console.log(password);
+    if (checkUser) {
+      const comparePass = await bcrypt.compare(password, checkUser.password);
+    } else if (checkShop) {
+      const comparePass2 = await bcrypt.compare(password, checkShop.password);
+    } else {
+      const comparePass3 = await bcrypt.compare(password, checkVet.password);
+    }
 
-        }else if(checkShop){
-          const comparePass2 = await bcrypt.compare(password, checkShop.password)
-        }else{
-          const comparePass3 = await bcrypt.compare(password, checkVet.password) 
-        }
-        
-        
-        
-        
+    if (checkUser) {
+      const accessToken = jwt.sign(
+        { userId: user.id, username: user.email },
+        ACCESS_TOKEN_SECRET,
+        { expiresIn: "2m" },
+      );
 
-        if(checkUser){
-            const accessToken = jwt.sign({ userId: user.id, username: user.email }, ACCESS_TOKEN_SECRET, { expiresIn: '2m' });
+      const refreshToken = jwt.sign(
+        { userId: user.id, username: user.email },
+        REFRESH_TOKEN_SECRET,
+        { expiresIn: "7d" },
+      );
 
-  
-            const refreshToken = jwt.sign({ userId: user.id, username: user.email }, REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
+      res.json({ accessToken, refreshToken, checkUser });
+    } else if (checkShop) {
+      const accessToken = jwt.sign(
+        { userId: shop.id, username: shop.email },
+        ACCESS_TOKEN_SECRET,
+        { expiresIn: "2m" },
+      );
 
-            res.json({ accessToken, refreshToken });
-            
-            
-              
-              
-            
+      const refreshToken = jwt.sign(
+        { userId: shop.id, username: shop.email },
+        REFRESH_TOKEN_SECRET,
+        { expiresIn: "7d" },
+      );
 
-              
-            
-        }else if(checkShop){
-          const accessToken = jwt.sign({ userId: shop.id, username: shop.email }, ACCESS_TOKEN_SECRET, { expiresIn: '2m' });
+      res.json({ accessToken, refreshToken, checkShop });
+    } else {
+      const accessToken = jwt.sign(
+        { userId: vet.id, username: vet.email },
+        ACCESS_TOKEN_SECRET,
+        { expiresIn: "2m" },
+      );
 
-  
-          const refreshToken = jwt.sign({ userId: shop.id, username: shop.email }, REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
+      const refreshToken = jwt.sign(
+        { userId: vet.id, username: vet.email },
+        REFRESH_TOKEN_SECRET,
+        { expiresIn: "7d" },
+      );
 
-          res.json({ accessToken, refreshToken });
-
-        } else{
-          const accessToken = jwt.sign({ userId: vet.id, username: vet.email }, ACCESS_TOKEN_SECRET, { expiresIn: '2m' });
-
-  
-          const refreshToken = jwt.sign({ userId: vet.id, username: vet.email }, REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
-
-          res.json({ accessToken, refreshToken });
-
-        }
-
-
-
-
-
-
-    }catch(error){console.log(error)}
-
-}
-
-
+      res.json({ accessToken, refreshToken });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 exports.forgotpassword = async (req, res) => {
-    const { email } = req.body;
+  const { email } = req.body;
 
   // Find the user by email (in a real app, this would query a database)
-  const user = await User.findOne({email});
+  const user = await User.findOne({ email });
 
   if (!user) {
-    return res.status(404).json({ error: 'User not found' });
+    return res.status(404).json({ error: "User not found" });
   }
 
   // Generate a reset token with user's ID
-  const resetToken = jwt.sign({ userId: user.id }, ACCESS_TOKEN_SECRET, { expiresIn: '1m' });
+  const resetToken = jwt.sign({ userId: user.id }, ACCESS_TOKEN_SECRET, {
+    expiresIn: "1m",
+  });
 
   // Here, you would typically send the reset token through email or another channel
-  
+
   console.log("Reset Token:", resetToken);
   /*
   return res.json({ message: 'Reset token sent successfully' });
   */
-}
-exports.resetpassword = async(req,res)=>{
-    const {email, newPassword } = req.body;
-    
+};
+exports.resetpassword = async (req, res) => {
+  const { email, newPassword } = req.body;
 
   // Verify the reset token
   jwt.verify(resetToken, ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      return res.status(401).json({ error: 'Invalid or expired token' });
+      return res.status(401).json({ error: "Invalid or expired token" });
     }
 
     // Token is valid, find the user by userId (in a real app, this would query a database)
-    const user = User.findOne({email});
+    const user = user.findOne({ email });
 
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
 
     // Update the user's password (in a real app, you'd update the database)
-    User.password = newPassword;
+    user.password = newPassword;
 
-    return res.json({ message: 'Password updated successfully' });
+    return res.json({ message: "Password updated successfully" });
   });
+};
 
-}
-   
-
-exports.resetPassword = async(req,res)=>{
+exports.resetPassword = async (req, res) => {
   try {
     const schema = Joi.object({ email: Joi.string().email().required() });
     const { error } = schema.validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
-    const user = await User.findOne({ email: req.body.email });
+    const user = await user.findOne({ email: req.body.email });
     if (!user)
-        return res.status(400).send("user with given email doesn't exist");
+      return res.status(400).send("user with given email doesn't exist");
 
     let token = await Token.findOne({ userId: user._id });
     if (!token) {
-        token = await new Token({
-            userId: user._id,
-            token: crypto.randomBytes(32).toString("hex"),
-        }).save();
+      token = await new Token({
+        userId: user._id,
+        token: crypto.randomBytes(32).toString("hex"),
+      }).save();
     }
 
     const link = `${process.env.BASE_URL}/password-reset/${user._id}/${token.token}`;
     await sendEmail(user.email, "Password reset", link);
 
     res.send("password reset link sent to your email account");
-} catch (error) {
+  } catch (error) {
     res.send("An error occured");
     console.log(error);
-}}
+  }
+};
 
-        
-  
+exports.signout = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    User.findByIdAndUpdate(user._id, { token: "" }, { new: true })
+      .then(updatedUser => {
+        if (!updatedUser) {
+          console.log("User not found");
+          return;
+        }
+        console.log("User updated with new token:", updatedUser);
+      })
+      .catch(error => {
+        console.error("Error updating user:", error);
+      });
+  } catch (error) {}
+};
 
-exports.signout = async(req,res)=>{
-    try{
-        const{ email, password } = req.body
-        const user = await User.findOne({ email })
-        User.findByIdAndUpdate(user._id, { token: "" }, { new: true })
-                .then(updatedUser => {
-                    if (!updatedUser) {
-                    console.log('User not found');
-                    return;
-                    }
-                    console.log('User updated with new token:', updatedUser);
-                })
-                .catch(error => {
-                    console.error('Error updating user:', error);
-                });
+exports.token = async (req, res) => {
+  try {
+    const { refreshToken } = req.body;
 
-    }catch(error){
-
-    }
-}
-
-
-
-
-exports.token = async(req,res)=>{
-    try{
-        const { refreshToken } = req.body;
-  
     if (!refreshToken) {
-      return res.status(401).json({ error: 'Refresh token is required' });
+      return res.status(401).json({ error: "Refresh token is required" });
     }
-  
+
     jwt.verify(refreshToken, REFRESH_TOKEN_SECRET, (err, decoded) => {
       if (err) {
-        return res.status(403).json({ error: 'Refresh token is not valid' });
+        return res.status(403).json({ error: "Refresh token is not valid" });
       }
-  
+
       // Assuming the refresh token is valid, generate a new access token
-      const accessToken = jwt.sign({ userId: decoded.userId, username: decoded.email }, ACCESS_TOKEN_SECRET, { expiresIn: '2m' });
-  
+      const accessToken = jwt.sign(
+        { userId: decoded.userId, username: decoded.email },
+        ACCESS_TOKEN_SECRET,
+        { expiresIn: "2m" },
+      );
+
       res.json({ accessToken });
     });
-    }catch(error){
-        res.status(400).json({error})
-    
-    }
-    
-} 
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
 
+exports.authenticateToken = async (req, res, next) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-exports.authenticateToken = async(req,res,next)=>{
-    try{
-        const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-  
     if (!token) {
-      return res.status(401).json({ error: 'Access token is required' });
+      return res.status(401).json({ error: "Access token is required" });
     }
-  
+
     jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
       if (err) {
-        return res.status(403).json({ error: 'Access token is not valid' });
+        return res.status(403).json({ error: "Access token is not valid" });
       }
       req.user = user;
       next();
-
     });
-    }catch(error){
-        res.status(400).json({error})
-    
-    }
-    
-} 
-
-
-
-
-
-
-
-
-
-
-
-
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -1,28 +1,22 @@
-const AuthController = require("../controllers/AuthController")
-const PostController = require("../controllers/PostController")
+const AuthController = require("../controllers/AuthController");
+const PostController = require("../controllers/PostController");
 const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
+router.post("/forgot", AuthController.forgotpassword);
+router.post("/token", AuthController.token);
+router.post("/signup", AuthController.signup);
+router.post("/shopsignup", AuthController.shopsignup);
+router.post("/vetsignup", AuthController.vetsignUp);
+router.post("/signin", AuthController.signin);
+router.post(
+  "/forgotPass",
+  AuthController.forgotpassword,
+  AuthController.resetpassword,
+);
+router.post("/signout", AuthController.signout);
 
+router.post("/post", AuthController.authenticateToken, PostController.addPost);
 
-
-
-
-
-router.post("/forgot", AuthController.resetPassword)
-router.post("/token", AuthController.token)
-router.post("/signup",AuthController.signup)
-router.post("/shopsignup", AuthController.shopsignup)
-router.post("/vetsignup",AuthController.vetsignUp)
-router.post("/signin", AuthController.signin)
-router.post("/forgotPass", AuthController.forgotpassword, AuthController.resetpassword)
-router.post("/signout", AuthController.signout)
-
-router.post("/post", AuthController.authenticateToken, PostController.addPost)
-
-
-
-
-
-module.exports = router
+module.exports = router;
