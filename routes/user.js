70261da67e0ce027a7710c8fce9d16820736e7b3
sherifyanni <163025160+sherifyanni@users.noreply.github.com>
2024-04-25 21:47:04 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,18 @@
-const AuthController = require("../controllers/AuthController")
-const PostController = require("../controllers/PostController")
+const AuthController = require("../controllers/AuthController");
+const PostController = require("../controllers/PostController");
+const Email = require("../utils/sendEmail")
 const express = require("express");
 const router = express.Router();
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
+router.post("/forgot", AuthController.forgotpassword);
+router.post("/token", AuthController.token);
+router.post("/signup", AuthController.signup);
+router.post("/shopsignup", AuthController.shopsignup);
+router.post("/vetsignup", AuthController.vetsignUp);
+router.post("/signin", AuthController.signin);
+router.post("/forgotPass", AuthController.forgotpassword, Email);
+router.post("/post", AuthController.authenticateToken, PostController.addPost);
+router.post("/reset/:token", AuthController.myReset)
 
-
-
-
-
-
-
-router.post("/token", AuthController.token)
-router.post("/signup",AuthController.signup)
-router.post("/signin", AuthController.signin)
-router.post("/forgot", AuthController.forgotpassword, AuthController.resetpassword)
-router.post("/signout", AuthController.signout)
-
-router.post("/post", AuthController.authenticateToken, PostController.addPost)
-
-
-
-
-
-module.exports = router
+module.exports = router;
