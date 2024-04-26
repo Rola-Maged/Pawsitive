7179b4315516a3a80e65e6070c677b6c48f834c9
sherifyanni <163025160+sherifyanni@users.noreply.github.com>
2024-04-26 20:44:00 +0300

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -321,7 +321,7 @@ exports.forgotpassword = async (req, res) => {
       subject: 'Password Reset',
       text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n`
           + `Please click on the following link, or paste this into your browser to complete the process:\n\n`
-          + `http://localhost:8000/reset/${resetToken}\n\n`
+          + `http://localhost:8000/api/reset/${resetToken}\n\n`
           + `If you did not request this, please ignore this email and your password will remain unchanged.\n`
   };
   
