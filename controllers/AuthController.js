80299c4d8849ce4fba1480d6ece2355a27552b59
Rola-Maged <163257079+Rola-Maged@users.noreply.github.com>
2024-05-28 15:06:54 +0300

    .

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -31,12 +31,15 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.signup = async (req, res) => {
-  userValidate()
   try {
     const { name, password, email, address, gender, age, phone,role } = req.body;
 
     // Check If The Input Fields are Valid
-    
+    if (!name || !password || !email || !address || !gender || !age || !phone) {
+      return res
+        .status(400)
+        .json({ message: "Please Input the Required Fields" });
+    }
 
     // Check If User Exists In The Database
     const existingUser = await user.findOne({ email });
