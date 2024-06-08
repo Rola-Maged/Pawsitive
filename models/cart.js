aebb885c192e74dffaa24604ee489fb8e3020e7f
Rola-Maged <163257079+Rola-Maged@users.noreply.github.com>
2024-06-09 00:49:14 +0300

    ..

--- models/cart.js
+++ models/cart.js
@@ -21,7 +21,6 @@ const cart = mongoose.model("cart", cartSchema);
  
     const cartJoischema = Joi.object({
         quantity: Joi.number().required(),
-        products: Joi.string().required(),
     });
      
     const cartValidate = (cart) => {
